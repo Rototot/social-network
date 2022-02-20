d3fe3bc304c308beda699853021a56050e83dc8a
Roman Zatonskiy <roman.zatonskiy@iqoption.com>
2022-02-20 22:15:52 +0300

    fix tests

--- backend/scripts/run-test-e2e.sh
+++ backend/scripts/run-test-e2e.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-#trap "docker-compose down -v --rmi local"
+trap "docker-compose down -v --rmi local"
 
 docker-compose -f docker-compose.tests.yaml build --pull
 docker-compose -f docker-compose.tests.yaml run tests-e2e
